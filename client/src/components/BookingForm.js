4c19126ed78d7c8bc0a36f2e654dee826de4c40d
Christina Kozanian <christinakozanian@gmail.com>
2024-05-22 21:00:38 -0700

    add formik and yup to confirmation page

--- client/src/components/BookingForm.js
+++ client/src/components/BookingForm.js
@@ -1,59 +1,99 @@
 import React, { useContext } from "react"
 import { useNavigate } from "react-router-dom";
 import { BookingContext } from "../contexts/BookingContext";
+import * as yup from 'yup'
+import { useFormik } from "formik";
 
 function BookingForm() {
     const navigate = useNavigate();
-    const { date, setDate, startTime, setStartTime,endTime, setEndTime, numberOfGuests, setNumberOfGuests } = useContext(BookingContext)
+    const { date, setDate, startTime, setStartTime, endTime, setEndTime, numberOfGuests, setNumberOfGuests } = useContext(BookingContext)
   
-    function handleSubmit(e) {
-      e.preventDefault();
-      navigate('/venues')
+    function handleSubmit(values) {
+      const selectedDate = new Date(values.date);
+      const selectedStartTime = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), ...values.startTime.split(':'));
+      const selectedEndTime = new Date(selectedDate.getFullYear(), selectedDate.getMonth(), selectedDate.getDate(), ...values.endTime.split(':'));
+  
+      setDate(selectedDate);
+      setStartTime(selectedStartTime);
+      setEndTime(selectedEndTime);
+      setNumberOfGuests(values.numberOfGuests)
+      // navigate('/venues')
+      console.log('date' +date,typeof(date), 'startTime' +startTime, typeof(startTime),'endTime' +endTime,typeof(endTime),'numberOfGuests' +numberOfGuests,typeof(numberOfGuests))
+    }
+
+    const schema = yup.object({
+      date: yup.date().min(new Date(), "Date must be after the today's date").required(),
+      startTime: yup.string().required(),
+      endTime: yup.string().required(),
+      numberOfGuests: yup.number().positive().required()
+    })
+
+    const formik = useFormik({
+      initialValues: {
+        date: "",
+        startTime: "",
+        endTime: "",
+        numberOfGuests: ""
+      },
+      validationSchema: schema,
+      onSubmit: handleSubmit
+    })
+
+    const displayErrors = (error) => {
+        return error ? <p style={{ color: "red" }}>{ error }</p> : null;
     }
 
     return (
-      <form onSubmit={handleSubmit}>
+      <form onSubmit={formik.handleSubmit}>
         {/* <formfeild> */}
           <label>Date: </label>
           <input
+            id='date'
             type='date'
             placeholder="Date"
-            value={date}
-            onChange={(e)=>setDate(e.target.value)}>
+            value={formik.values.date}
+            onChange={ formik.handleChange }>
           </input>
+          { displayErrors(formik.errors.date) }
         {/* </formfeild> */}
         <br />
         <br />
         {/* <formfeild> */}
           <label>Start Time: </label>
           <input
+            id='startTime'
             type='time'
             placeholder="Start Time"
-            value={startTime}
-            onChange={(e)=>setStartTime(e.target.value)}>
+            value={formik.values.startTime}
+            onChange={ formik.handleChange }>
           </input>
+          { displayErrors(formik.errors.startTime) }
         {/* </formfeild> */}
         <br />
         <br />
         {/* <formfeild> */}
           <label>End Time: </label>
           <input
+            id='endTime'
             type='time'
             placeholder="End Time"
-            value={endTime}
-            onChange={(e)=>setEndTime(e.target.value)}>
+            value={formik.values.endTime}
+            onChange={ formik.handleChange }>
           </input>
+          { displayErrors(formik.errors.endTime) }
         {/* </formfeild> */}
         <br />
         <br />
         {/* <formfeild> */}
           <label>Number of Guests: </label>
           <input
+            id='numberOfGuests'
             type='number'
             placeholder="Number of guests"
-            value={numberOfGuests}
-            onChange={(e)=>setNumberOfGuests(e.target.value)}>
+            value={formik.values.numberOfGuests}
+            onChange={ formik.handleChange }>
           </input>
+          { displayErrors(formik.errors.numberOfGuests) }
         {/* </formfeild> */}
         <br />
         <br />
